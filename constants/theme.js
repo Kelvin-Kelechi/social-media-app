82a100875a2d75a533f1be9220b15d3d40561711
KELEX <ogkelvin99@gmail.com>
2024-11-10 04:56:48 +0100

    setting up app

---
+++ constants/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    primary: "#00C26F",
+    primaryDark: "#00AC262",
+    dark: "#3E3E3E",
+    darkLight: "#E1E1E1",
+    gray: "#e3e3e3e",
+
+    text: "#00C26F",
+    textLight: "#00C26F",
+    textDark: "#00C26F",
+
+    rose: "#ef4444",
+    roseLight: "#f8717171",
+  },
+  fonts: {
+    medium: "500",
+    semiBold: "600",
+    bold: "700",
+    extraBold: "800",
+  },
+  radius: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    xl: 16,
+    xxl: 18,
+  },
+};
